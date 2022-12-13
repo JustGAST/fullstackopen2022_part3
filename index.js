5c4764b164ccb4ba1f7f0f52e3705304c1ebd376
justgast <justgast@gmail.com>
2022-12-13 17:35:08 +0300

    Part 3: Exercises 3.3 - 3.5 ready;

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-const notes = [
+let persons = [
   {
     "id": 1,
     "name": "Arto Hellas",
@@ -26,12 +27,57 @@ const notes = [
 ];
 
 app.get('/info', (req, res) => {
-  const entries = notes.length;
+  const entries = persons.length;
   const time = new Date().toString();
 
   res.send(`Phonebook has info for ${entries} people.<br><br>${time}`)
+});
+
+app.get('/api/persons', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (!person) {
+    res.statusMessage = 'No person with such id';
+    res.status(404).end();
+
+    return;
+  }
+
+  res.json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+  const {name, number} = req.body;
+
+  if (!name || !number) {
+    res.status(400).end();
+
+    return;
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 9999999),
+    name,
+    number,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
 })
-app.get('/api/notes', (req, res) => res.json(notes));
 
 const PORT = 3001;
 app.listen(PORT, () => {
